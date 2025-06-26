6ad171ef7d3d3da2be9c93b140145352c59405ce
LCIMS1 <olkaturskaya@gmail.com>
2025-06-26 12:53:03 +0300

    Loop Continue

--- main.py
+++ main.py
@@ -35,4 +35,4 @@ while count < 5 :
     count += 1
     if count == 3 :
         continue  # прервать текущую итерацию и начать следующую
-print(f'Итерация номер: {count}')
+    print(f'Итерация номер: {count}')
