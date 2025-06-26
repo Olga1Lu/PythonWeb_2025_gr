798bedf9cc541e7225921906d373f64d0aab44ae
LCIMS1 <olkaturskaya@gmail.com>
2025-06-26 14:36:03 +0300

    Mathc Case

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 # Mathc Case (3.10)
-from re import match
+# from re import match
+#
+# print('Возможные ходы:\n\tL - влево\n\tR - вправо\n\tF - прямо')
+#
+# while True :
+#     ch = input('ваш выбор: ')
+#     match ch:
+#         case 'L' | 'l' | 'д' | 'Д' :
+#             print('Свернули влево')
+#         case 'К' | 'k' | 'к' | 'К':
+#             print('Свернули вправо')
+#         case 'F' | 'f' | 'ф' | 'Ф':
+#             print('Свернули влево')
+#         case 'F' | 'f' | 'ф' | 'Ф':
+#             print('До свидания!')
+#             break
+#         case _:
+#             print('Свернули влево')
 
-print('Возможные ходы:\n\tL - влево\n\tR - вправо\n\tF - прямо')
+count = 1
 
-while True :
-    ch = input('ваш выбор: ')
-    match ch:
-        case 'L' | 'l' | 'д' | 'Д' :
-            print('Свернули влево')
-        case 'К' | 'k' | 'к' | 'К':
-            print('Свернули вправо')
-        case 'F' | 'f' | 'ф' | 'Ф':
-            print('Свернули влево')
-        case 'F' | 'f' | 'ф' | 'Ф':
-            print('До свидания!')
-            break
-        case _:
-            print('Свернули влево')
+while count <= 100 :
+
+    if count % 10 == 3 :
+ #   match count :
+  #      case
+        print(count)
+        count += 1
