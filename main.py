d03ed2692106f577830156e9e8ad4bce7cb30651
LCIMS1 <olkaturskaya@gmail.com>
2025-06-26 14:28:36 +0300

    Mathc Case

--- main.py
+++ main.py
@@ -1,62 +1,19 @@
-# Циклы:
+# Mathc Case (3.10)
+from re import match
 
+print('Возможные ходы:\n\tL - влево\n\tR - вправо\n\tF - прямо')
 
-# цикл до ввода пустой строки
-# 1 без моржа
-# word = input('Введите слово: ')
-#
-# while word != '' :
-#     print(f'Слово: "{word}')
-#     word = input('Введите слово: ')
-#
-# print('строка пустая')
-
-# break
-# continue
-
-# num = 3  # надо угадать его
-# var = ''
-#
-# print('Угадайте число')
-#
-# while True :
-#     var = int( input('Ваше значение: '))
-#     if var == num :
-#         print('угадали!')
-#         break  # фцикл прерван
-#     elif var > num :
-#         print('Число больше загаданного')
-#     else:
-#         print('Число меньше загаданного')
-#
-# print('Приходи еще')
-# count = 0
-# while count < 5 :
-#     count += 1
-#     if count == 3 :
-#         continue  # прервать текущую итерацию и начать следующую
-#     print(f'Итерация номер: {count}')
-
-
-h = 0
-
-# while h <= 150 or h >= 180 :
-#     h = int(input('Укажите Ваш рост в см: '))
-#     if h > 150 and h < 180 :
-#         print('Подходит.')
-#         break
-#     else:
-#         print('Не подходит')
-# h = int(input('Укажите Ваш рост в см: '))
-# while h <= 150 or h >= 180 :
-#     print('Не подходит.')
-#     h = int(input('Укажите Ваш рост в см: '))
-#
-# print('Подходит')
-
-h = int(input('Укажите Ваш рост в см: '))
-
-while not (150 <= h <= 180) :
-    print(f'Рост {h} не подходит')
-    h = int(input('Укажите Ваш рост в см: '))
-print('Подходит')
+while True :
+    ch = input('ваш выбор: ')
+    match ch:
+        case 'L' | 'l' | 'д' | 'Д' :
+            print('Свернули влево')
+        case 'К' | 'k' | 'к' | 'К':
+            print('Свернули вправо')
+        case 'F' | 'f' | 'ф' | 'Ф':
+            print('Свернули влево')
+        case 'F' | 'f' | 'ф' | 'Ф':
+            print('До свидания!')
+            break
+        case _:
+            print('Свернули влево')
