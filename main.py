807d53c43d08a0c3f9152422e5727be62192ccc5
LCIMS1 <olkaturskaya@gmail.com>
2025-06-26 12:52:29 +0300

    Loop Continue

--- main.py
+++ main.py
@@ -14,19 +14,25 @@
 # break
 # continue
 
-num = 3  # надо угадать его
-var = ''
-
-print('Угадайте число')
-
-while True :
-    var = int( input('Ваше значение: '))
-    if var == num :
-        print('угадали!')
-        break  # фцикл прерван
-    elif var > num :
-        print('Число больше загаданного')
-    else:
-        print('Число меньше загаданного')
-
-print('Приходи еще')
+# num = 3  # надо угадать его
+# var = ''
+#
+# print('Угадайте число')
+#
+# while True :
+#     var = int( input('Ваше значение: '))
+#     if var == num :
+#         print('угадали!')
+#         break  # фцикл прерван
+#     elif var > num :
+#         print('Число больше загаданного')
+#     else:
+#         print('Число меньше загаданного')
+#
+# print('Приходи еще')
+count = 0
+while count < 5 :
+    count += 1
+    if count == 3 :
+        continue  # прервать текущую итерацию и начать следующую
+print(f'Итерация номер: {count}')
