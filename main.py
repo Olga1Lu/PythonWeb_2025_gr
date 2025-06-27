a8103485a0b464c16fcb7298ee8c371dfcc35442
LCIMS1 <olkaturskaya@gmail.com>
2025-06-27 11:28:32 +0300

    Set Intro

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
+# Коллекции
+# Множества, списки, словари, коллекции
 
-total = 0
-total_suc = 0
-# total_ensuc = 0
-min_v = float('inf')
-max_v = float ('-inf')
+# множества
 
-while (num := int(input('Введите рост: '))) != -1 :
-    if 150 <= num <= 180:
-        total_suc += 1
-        if num < min_v :
-            min_v = num
-        if num > max_v :
-            max_v = num
+s = set()  # Пустое
+s = {'3' , '5' , '7' , 3 , 10, '3'}
+# print(s)
+# s.add('8.5')
+# print(s)
+# print('Есть ли 3 ?')
+# if str(3) in s:
+#     print('Да')
+# for item in s :
+#     if item == '3' :
+#         print(item)
+# print(f'Число элементов: {len(s)}')
 
-    total += 1
-print(f'Всего кандидатов: {total}')
-print(f'Успешных: {total_suc}, неуспешных: {total - total_suc}')
-print(f'Макс.рост: {max_v}')
-print(f'Мин.рост: {min_v}')
+# print(dir(s))
+
+#'add', 'clear', 'copy', 'difference', 'difference_update', 'discard',
+ #'intersection', 'intersection_udate'
+# 'isdisjoint', 'issubset', 'issuperset', 'pop', 'remove', 'symmetric_difference',
+# 'symmetric_difference_update','union', 'update']
+
+#удаление эл-тов  из множества
+s.remove('3')  # вызывает ошибку, если эл-та нет
+s.discard('3')  # удаляет вслепую
+temp = s.pop()  # удаляет случайный эл-т и вовращает его, если пустое мн-во, то er
+print(temp)
+# s.clear()  # очистка полностью
+for item in s :
+    print(item)
+
+
+
+
+
 
