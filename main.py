315730835356e1364c37cae80205f2ea75758d72
LCIMS1 <olkaturskaya@gmail.com>
2025-07-08 14:38:03 +0300

    Tasks Clever Dictionary

--- main.py
+++ main.py
@@ -141,3 +141,55 @@ from logging import exception
 #     else:
 #         print(res)
 #         break
+
+# Обучаемый словарь - проверить ошибки
+# import pickle
+#
+# # мин. версия, если файл dict.dat отсутствует
+# voc = {
+#     'стол': 'table',
+#     'стул': 'chair',
+# }
+#
+# def print_voc():  # распечатка словаря
+#     print('Сейчас словарь содержит: ')
+#     for k,v in voc.items() :
+#         print(k,  '-', v)  # Alt + 0151
+# # загружаем словарь из файла
+# try:
+#     with open('dict.dat', 'rb') as dump_in:
+#         voc = pickle.load(dump_in)
+# except FileNotFoundError :
+#     with open('dict.dat', 'wb') as dump_out:
+#         pickle.dump(voc, dump_out)
+#     print('Создан минимальный словарь')
+#     print_voc()
+#
+# while True:
+#     temp = input('\nВведите слово для перевода или "#" для завершения: ')
+#     word = temp.strip().lower()
+#     if word == '#' or word == '№':
+#         break
+#     if word in voc.keys():
+#         translate = voc[word]
+#         print(f'Слово "{word}" переводится как {translate}.\n')
+#     else:
+#         print(f'Значение слова {word} отсутствует в словаре.')
+#         newkey = f'А как слово {word} переводится? '
+#         newkey += 'Если ничего не вводите, нажмите ENTER, '
+#         newkey += 'или введите его здесь: '
+#         new_word = input(newkey)
+#
+#         if new_word != '' or len(new_word) > 2:
+#             voc[word] = new_word
+#             print(f'Слово {word} с переводом {new_word} внесено в словарь')
+#         else:
+#             print('Ничего не введено или слишком короткое слово')
+#             continue
+#
+# # сохранить словарь
+#
+# with open('dict.dat', 'wb') as dump_out :
+#     pickle.dump(voc, dump_out)
+#
+# print('До новых встреч!')
