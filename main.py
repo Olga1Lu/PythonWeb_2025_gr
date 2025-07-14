de8eb5f474dad164fe333b1247c499c3abf2d05f
LCIMS1 <olkaturskaya@gmail.com>
2025-07-14 15:43:04 +0300

    JSON read, write

--- main.py
+++ main.py
@@ -650,14 +650,74 @@ from urllib3.filepost import writer
 #     writer.writerow(data)
 
 # режимы квотирования
-data = ['name', 25, 'town']
-with open('sample.csv', 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)  # надо записать все, но числа в кавычки не заключать
-    writer.writerow(data)
+# data = ['name', 25, 'town']
+# with open('sample.csv', 'w', newline='', encoding='utf-8') as f:
+#     writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)  # надо записать все, но числа в кавычки не заключать
+#     writer.writerow(data)
+########################################
+# Zip
+# from zipfile import ZipFile
+# import os
+#
+# csv_files = [f for f in os.listdir() if f.endswith('.csv')]
+# # print(csv_files)
+# with ZipFile('archive.zip', 'w') as myzip:
+#     for file in csv_files:
+#         myzip.write(file)
+#         os.remove(file)
+# with ZipFile('archive.zip', 'r') as zip_obj:
+#     zip_obj.extractall()
+
+
+# file_to_extract = ['people.csv', 'file.csv']
+# with ZipFile('archive.zip', 'r') as zip_obj:
+#     zip_obj.extractall(members=file_to_extract)  # распаковать только отдельные файлы
+
+# with ZipFile('archive.zip', 'r') as zip_obj:
+#     print(zip_obj.namelist())  # список файлов в архиве
+########################################
+
+# JSON (Java Script Object Notation)
+#load - для чтения из файла
+#loads - для чтения данных в строковом представлении - данные прямо в этом же файле
+import json
+
+#чтение данных
+# with open('dogs.json', 'rt') as d:
+#     #data = json.load(d)  # напрямую из файла
+#     temp = d.read()  # прочитали файл как строку
+#     data = json.loads(temp)  # сделали словарь из строки, читая построчно
+#     print(data)
+#
+# for i in range(len(data)):  # для нескольких, также можно enumerate
+#     print(f'i+1')
+#     for k, v in data[i].items():
+#         if type(v) == list:
+#             print(f'\t{k}: {', '.join(v)}')
+#         else:
+#             print(f'\t{k}: {v}')
+#
+    # for k,v in data.items():
+    #     if type(v) == list:
+    #         print(f'{k}: {', '.join(v)}')
+    #     else:
+    #         print(f'{k}: {v}')
 
 
 
+# запись
 
+d = {
+    'ананас': 300,
+    'банан': 400,
+    'яблоко': 120,
+    'груша': 280,
+}
+
+# with open('fruits.json', 'wt', encoding='utf-8') as f:
+    #json.dump(d,f, indent=4)  #запись в файл с отступом 4, для читабельности
+data = json.dumps(d, indent=4)  #вывод в виде строки
+print(data)
 
 
 
