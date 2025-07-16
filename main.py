7a531cd9950ef8b1253f7b2baea449ea3cff0492
LCIMS1 <olkaturskaya@gmail.com>
2025-07-16 12:48:37 +0300

    URL parameters

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from fileinput import filename
 from http.cookiejar import debug
 
 from flask import Flask, url_for
+import sqlite3
+
+from urllib3.util.proxy import connection_requires_http_tunnel
 
 app = Flask(__name__)  # регистрируем приложение
 debug = False
@@ -41,6 +44,48 @@ def sample_page():
 </body>
 </html>
     """
+
+@app.route('/sample-page2')
+def sample_page2():
+    with open('temp.html', 'r', encoding='utf-8') as html:
+        return html.read()
+###################
+#не рекомендуется
+# x = 5
+# @app.route('/1')
+# def show_num():
+#     global x
+#     x += 1
+#     return str(x)
+###################
+
+# типы конверторов - по умолчанию всегда строка
+#<string> по умолчанию строка
+#<int:number> - целое
+#<float:number> - вещ.числа
+#<path:p> - может содержать слэши для указания пути
+#<uuid:id> - строка идентификатор (16 байт в HEX-формате)
+
+# передать данные, определяемые пользователем
+@app.route('/greeting/<user>/<int:id_num>')  # вместо <> м.подставить любое слово
+def greeting(user, id_num):
+    return f'Привет, {user} c id= {id_num}'
+
+#обращение к базе данных
+@app.route('/get-user/<int:id_num>')  # вместо <> м.подставить любое слово
+def get_user(id_num):
+
+    con = sqlite3.connect('db/movies.sqlite')
+    cur = con.cursor()
+    query = f'SELECT name FROM users WHERE trip_id={id_num}'
+    response = cur.execute(query)
+    result = response.fetchone()
+    #print(result)  # получили объект
+
+    cur.close()  # подтверждение
+    con.close()  # закрываем подключение
+    return  str(result[0])
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=debug)
